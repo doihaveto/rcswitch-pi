050340702c59efca0fefcc1ea88206aa7b1e7e90
doihaveto <lidor@randomlab.info>
2015-01-29 20:28:03 +0200

    This version works

--- send.cpp
+++ send.cpp
@@ -8,32 +8,26 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    
+    if (argc != 4) {
+        printf("Usage: %s <decimal code> <bit length> <pulse length>\n", argv[0]);
+        return 1;
+    }
     /*
      output PIN is hardcoded for testing purposes
      see https://projects.drogon.net/raspberry-pi/wiringpi/pins/
      for pin mapping of the raspberry pi GPIO connector
      */
-    int PIN = 0;
-    char* systemCode = argv[1];
-    int unitCode = atoi(argv[2]);
-    int command  = atoi(argv[3]);
+    int PIN = 5;
+    unsigned int decimalCode = atoi(argv[1]); // light: 10434200
+    int bitLength = atoi(argv[2]); // light: 24
+    int pulseLength = atoi(argv[3]); // light: 130
+    if (wiringPiSetup () == -1)
+        return 1;
+
+    RCSwitch mySwitch = RCSwitch();
+    mySwitch.enableTransmit(PIN);
+    mySwitch.setPulseLength(pulseLength);
     
-    if (wiringPiSetup () == -1) return 1;
-	printf("sending systemCode[%s] unitCode[%i] command[%i]\n", systemCode, unitCode, command);
-	RCSwitch mySwitch = RCSwitch();
-	mySwitch.enableTransmit(PIN);
-    
-    switch(command) {
-        case 1:
-            mySwitch.switchOn(systemCode, unitCode);
-            break;
-        case 0:
-            mySwitch.switchOff(systemCode, unitCode);
-            break;
-        default:
-            printf("command[%i] is unsupported\n", command);
-            return -1;
-    }
-	return 0;
+    mySwitch.send(decimalCode, bitLength);
+    return 0;
 }
